19db3c76b3880af58f0bf3834edab35785a42324
webpig <11765461@qq.com>
2020-01-11 01:53:11 +0800

    feat: quickSortInPlace

---
+++ sort/quickSortInPlace.js
@@ -0,0 +1,27 @@
+function quickSortInPlace (arr, startIndex, endIndex) {
+    if (startIndex >= endIndex) return 
+    
+    let pivot = partition(arr, startIndex, endIndex)
+    quickSortInPlace(arr, startIndex, pivot - 1)
+    quickSortInPlace(arr, pivot + 1, endIndex)
+}
+
+function partition (arr, startIndex, endIndex) {
+    let pivot = arr[endIndex]
+    let i = startIndex
+
+    for (let j = startIndex; j < endIndex; j++) {
+        if (arr[j] < pivot) {
+            [arr[i], arr[j]] = [arr[j], arr[i]]
+            i++
+        }
+    }
+
+    [arr[i], arr[endIndex]] = [arr[endIndex], arr[i]]
+    
+    return i
+}
+
+module.exports = {
+    quickSortInPlace
+}
